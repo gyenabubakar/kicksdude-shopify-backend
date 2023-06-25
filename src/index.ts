19d7de720c13ab5d6f3eee1c7892ab12c5452694
Gyen Abubakar <indehyde@gmail.com>
2023-06-25 05:08:22 +0000

    updated console log

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running in http://localhost:${PORT}`);
+  console.log(`👉👉👉Server is running in http://localhost:${PORT}`);
 });
